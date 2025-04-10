4e33df6048e006a9bb4fa4837e8b6d0a974bd81c
lishuailibertine <libertine9458@gmail.com>
2025-04-10 18:34:03 +0800

    改为多入口

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,31 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   publicPath: './',
   outputDir: 'dist', // 输出目录
+  pages: {
+    app: {
+      entry: 'src/main.js',
+      template: 'public/app/index.html',
+      filename: 'index.html'
+    },
+    requestLogin: {
+      entry: 'src/requestLogin/requestLogin.js',
+      template: 'public/requestLogin/requestLogin.html',
+      filename: 'requestLogin.html'
+    },
+  },
+  css: {
+    extract: {
+      filename: "css/[name].css",          // 输出 css 路径
+      chunkFilename: "css/[name].css"
+    }
+  },
   configureWebpack: {
     output: {
-      filename: '[name].js'
+      filename: "js/[name].js",            // 输出 js 路径
+      chunkFilename: "js/[name].js"
+    },
+    optimization: {
+      splitChunks: false 
     },
     optimization: {
       minimize: false, 
